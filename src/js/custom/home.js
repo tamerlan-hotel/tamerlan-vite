4bedeef9f485a3780b93f1cd1f6b882127e6263e
Danny-Lenko <danylenko.14@gmail.com>
2025-02-19 15:11:22 +0200

    try another method centering text onload

--- src/js/custom/home.js
+++ src/js/custom/home.js
@@ -1,8 +1,14 @@
 import footer from "./footer";
 import i18 from "./i18next-utils";
 
-(function () {
-  const slider = jQuery("#slider-revolution");
+// (function () {
+//   const slider = jQuery("#slider-revolution");
+//   slider.revnext();
+//   console.log("Home page loaded", slider);
+// })();
 
+window.onload = function () {
+  const slider = jQuery("#slider-revolution");
   slider.revnext();
-})();
+  console.log("Home page loaded", slider);
+};
