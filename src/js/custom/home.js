118ea06a9fa454077fa9ba13f9bb00546f4589ea
Danny-Lenko <danylenko.14@gmail.com>
2025-02-19 18:21:31 +0200

    test mutation observer to center

--- src/js/custom/home.js
+++ src/js/custom/home.js
@@ -1,30 +1,43 @@
 import footer from "./footer";
 import i18 from "./i18next-utils";
 
-// window.addEventListener("load", () => {
-//   setTimeout(() => {
-//     const slider = jQuery("#slider-revolution");
-//     slider.revnext();
-//   }, 770);
-// });
-
-window.addEventListener("load", () => {
-  function centerCaptions() {
-    document
-      .querySelectorAll(".slider-caption-1, .slider-caption-sub-1")
-      .forEach((el) => {
-        const parent = el.parentElement;
-        const parentWidth = parent.offsetWidth;
-        const elWidth = el.offsetWidth;
-        console.log("parentWidth:", parentWidth);
-        const newLeft = (parentWidth - elWidth) / 2;
-        el.style.left = `${newLeft}px`;
-      });
-  }
-
-  // Викликаємо після завантаження сторінки
-  centerCaptions();
-
-  // Також можна викликати після зміни розміру вікна
-  //   window.addEventListener("resize", centerCaptions);
+const observer = new MutationObserver(() => {
+  document
+    .querySelectorAll(".slider-caption-1, .slider-caption-sub-1")
+    .forEach((el) => {
+      const parent = el.parentElement;
+      const parentWidth = parent.offsetWidth;
+      const elWidth = el.offsetWidth;
+      console.log("PARENT WIDTH:", parentWidth);
+      const newLeft = (parentWidth - elWidth) / 2;
+      el.style.left = `${newLeft}px`;
+    });
 });
+
+// Спостерігаємо за змінами в стилях
+document
+  .querySelectorAll(".slider-caption-1, .slider-caption-sub-1")
+  .forEach((el) => {
+    observer.observe(el, { attributes: true, attributeFilter: ["style"] });
+  });
+
+// window.addEventListener("load", () => {
+//   function centerCaptions() {
+//     document
+//       .querySelectorAll(".slider-caption-1, .slider-caption-sub-1")
+//       .forEach((el) => {
+//         const parent = el.parentElement;
+//         const parentWidth = parent.offsetWidth;
+//         const elWidth = el.offsetWidth;
+//         console.log("parentWidth:", parentWidth);
+//         const newLeft = (parentWidth - elWidth) / 2;
+//         el.style.left = `${newLeft}px`;
+//       });
+//   }
+
+//   // Викликаємо після завантаження сторінки
+//   centerCaptions();
+
+//   // Також можна викликати після зміни розміру вікна
+//   //   window.addEventListener("resize", centerCaptions);
+// });
