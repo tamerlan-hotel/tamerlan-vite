91fc59f22df6e792b7487ae6d828a3d32b44fc58
Danny-Lenko <danylenko.14@gmail.com>
2025-02-19 18:15:08 +0200

    test js forcing elements to the center

--- src/js/custom/home.js
+++ src/js/custom/home.js
@@ -1,14 +1,30 @@
 import footer from "./footer";
 import i18 from "./i18next-utils";
 
-// (function () {
-//   const slider = jQuery("#slider-revolution");
-//   slider.revnext();
-//   console.log("Home page loaded", slider);
-// })();
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     const slider = jQuery("#slider-revolution");
+//     slider.revnext();
+//   }, 770);
+// });
 
-window.onload = function () {
-  const slider = jQuery("#slider-revolution");
-  slider.revnext();
-  console.log("Home page loaded", slider);
-};
+window.addEventListener("load", () => {
+  function centerCaptions() {
+    document
+      .querySelectorAll(".slider-caption-1, .slider-caption-sub-1")
+      .forEach((el) => {
+        const parent = el.parentElement;
+        const parentWidth = parent.offsetWidth;
+        const elWidth = el.offsetWidth;
+        console.log("parentWidth:", parentWidth);
+        const newLeft = (parentWidth - elWidth) / 2;
+        el.style.left = `${newLeft}px`;
+      });
+  }
+
+  // Викликаємо після завантаження сторінки
+  centerCaptions();
+
+  // Також можна викликати після зміни розміру вікна
+  //   window.addEventListener("resize", centerCaptions);
+});
